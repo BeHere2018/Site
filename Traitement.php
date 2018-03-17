eb0daad210de6b78800e73609208d5b9422760f9
AlexandreCorre <alexandre.corre@isep.fr>
2018-03-17 19:57:25 +0100

    sans le coucou

--- Traitement.php
+++ Traitement.php
@@ -6,7 +6,6 @@ if( isset($_COOKIE['id']) && isset($_POST['idbiere']) && isset($_POST['note']) )
         include $modele.'get_personne.php';
         $idP=get_personne_byToken($_COOKIE['id'])[0];
         print($idP);
-        echo "coucou";
         include_once $modele.'update_Xp.php';
         update_Xp($idP);
         put_rate($idP[0],$_POST['idbiere'],$_POST['Arome'],$_POST['Gout'],$_POST['Apparence'],$_POST['note']);
